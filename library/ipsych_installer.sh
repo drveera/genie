5bc9c57d25d64321350f183a50afdd83c9f1ff55
drveera <drveerammc@gmail.com>
2017-03-29 14:12:08 +0200

    update installer.sh

--- library/ipsych_installer.sh
+++ library/ipsych_installer.sh
@@ -7,6 +7,6 @@ echo "ssl_verify: /com/etc/ssl-proxy-cert.pem" > ~/.condarc
 conda create -n genie python=3.6 docopt=0.6.2
 source activate genie
 pip install --cert /com/etc/ssl-proxy-cert.pem snakemake==3.11.2
-echo "alias genie='/project/IGdata/faststorage/userdata/iveera/pipelines/genie/genie.py' >> ~/.bashrc"
+echo "alias genie='/project/IGdata/faststorage/userdata/iveera/pipelines/genie/genie.py'" >> ~/.bashrc
 source ~/.bashrc
 echo "done!"
