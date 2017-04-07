2efeca2c70de9cb959e7b3c064a4cb00b79384d6
drveera <drveerammc@gmail.com>
2017-04-07 19:20:36 +0200

    updated dbsnp

--- modules/sumstats/dbsnp/create.avinput2.R
+++ modules/sumstats/dbsnp/create.avinput2.R
@@ -6,15 +6,14 @@ anno1 <- args[1]
 outname <- args[2]
 
 library(data.table)
-if (! file.info(anno1)$size == 0){
-    system("echo something left to annotate >> av2file")
-    anno1 <- fread(anno1, header = TRUE, colClasses = "character")
-    avinput2 <- anno1[is.na(avsnp147)]
-    avinput2$End <- as.numeric(as.character(avinput2$Start)) + (nchar(avinput2$Alt)-1)
-    avinput2 <- avinput2[,c(1,2,3,5,4,6),with=FALSE]
-    write.table(avinput2, outname, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
-} else {
-    system("echo Nothing left to annotate >> av2file")
-    write(text = "a",outname)
+system("echo something left to annotate >> av2file")
+anno1 <- fread(anno1, header = TRUE, colClasses = "character")
+avinput2 <- anno1[is.na(avsnp147)]
+avinput2$End <- as.numeric(as.character(avinput2$Start)) + (nchar(avinput2$Alt)-1)
+avinput2 <- avinput2[,c(1,2,3,5,4,6),with=FALSE]
+write.table(avinput2, outname, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t")
+
+if (! file.info(outname)$size == 0){
+    write(text = "Nothing left to annotate", outname, append = TRUE)
 }
 
