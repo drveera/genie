f2b0412b62eb39a24aa594799d9974272b739da3
drveera <drveerammc@gmail.com>
2017-04-07 19:16:56 +0200

    updated dbsnp

--- modules/sumstats/dbsnp/create.avinput2.R
+++ modules/sumstats/dbsnp/create.avinput2.R
@@ -7,6 +7,7 @@ outname <- args[2]
 
 library(data.table)
 if (! file.info(anno1)$size == 0){
+    system("echo something left to annotate >> av2file")
     anno1 <- fread(anno1, header = TRUE, colClasses = "character")
     avinput2 <- anno1[is.na(avsnp147)]
     avinput2$End <- as.numeric(as.character(avinput2$Start)) + (nchar(avinput2$Alt)-1)
