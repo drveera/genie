0d9db09e531cd2e3c6c7cd4670f190724ffffea7
drveera <drveerammc@gmail.com>
2017-04-12 12:06:38 +0200

    changed width of plot

--- modules/prs/hub/hub.summary.R
+++ modules/prs/hub/hub.summary.R
@@ -25,4 +25,6 @@ p1 <- ggplot(sumdfm,aes(threshold,R2, group = pheno)) + geom_line(alpha = 0.1) +
     theme(axis.text.x=element_text(angle=90, hjust=1)) +
     labs(x = "P Value Thresholds", y = "Nagelkerke-R-Squared", title = paste0(basename(outname),"r2.plot")) +
     geom_line(data = sumpheno, aes(colour = pheno))
-ggsave(paste0(outname,"rsquaredValues.allpheno.pdf"))
+
+ggsave(paste0(outname,"rsquaredValues.allpheno.pdf"), width = 8, height = 4)
+#ggsave("temp.pdf", width = 8, height = 4)
