357f1c25290cecda45d58088627993f696b1e66e
drveera <drveerammc@gmail.com>
2017-04-12 14:45:29 +0200

    thinned pheno names

--- modules/prs/hub/hub.heatmap.R
+++ modules/prs/hub/hub.heatmap.R
@@ -18,6 +18,20 @@ for(i in 1:length(sumfiles)){
 }
 #sumlist <- lapply(sumlist, function(x) x[x$R2 == max(x$R2),])
 sumdfm <- do.call(rbind, sumlist)
+mf <- function(x,sep){
+    xsplit = strsplit(x,split = sep)
+    xdfm <- as.data.frame(do.call(rbind,xsplit))
+    res <- list()
+    for (i in 1:ncol(xdfm)){
+        if (!all(xdfm[,i] == xdfm[1,i])){
+            res[[length(res)+1]] <- as.character(xdfm[,i])
+        }
+    }
+    res <- as.data.frame(do.call(rbind,res))
+    res <- apply(res,2,function(x) paste(x,collapse="_"))
+    return(res)
+}
+sumdfm$pheno <- mf(sumdfm$pheno,sep = "_")
 
 p1 <- ggplot(sumdfm, aes(threshold,pheno)) + geom_tile(aes(fill = R2)) +
     theme(axis.text.x=element_text(angle=90, hjust=1)) +
