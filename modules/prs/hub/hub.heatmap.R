b24b176a8a3daf347b9cdf71abcbcc7b70560add
drveera <drveerammc@gmail.com>
2017-04-13 10:53:14 +0200

    order of thresholds in heatmap

--- modules/prs/hub/hub.heatmap.R
+++ modules/prs/hub/hub.heatmap.R
@@ -18,7 +18,9 @@ for(i in 1:length(sumfiles)){
 }
 #sumlist <- lapply(sumlist, function(x) x[x$R2 == max(x$R2),])
 sumdfm <- do.call(rbind, sumlist)
-
+threshold <- as.character(sumdfm$threshold)
+threshold <- threshold[!duplicated(threshold)]
+sumdfm$threshold <- factor(sumdfm$threshold, levels = threshold[mixedorder(threshold)])
 p1 <- ggplot(sumdfm, aes(threshold,pheno)) + geom_tile(aes(fill = R2)) +
     theme(axis.text.x=element_text(angle=90, hjust=1)) +
     scale_fill_gradient(low = "white", high = "red")
