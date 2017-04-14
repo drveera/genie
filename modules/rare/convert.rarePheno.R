c4a5b86029ac9d0657a433167ca9d0cb8a018a8a
drveera <drveerammc@gmail.com>
2017-04-14 22:50:13 +0200

    pheno script

---
+++ modules/rare/convert.rarePheno.R
@@ -0,0 +1,21 @@
+#!/bin/env Rscript
+
+args <- commandArgs(trailingOnly = TRUE)
+phenofile <- args[1]
+outname <- args[2]
+
+library(data.table)
+pheno <- fread(phenofile,header = FALSE)
+if(any(grepl("fid",unlist(pheno[1,]), ignore.case = TRUE))){
+    names(pheno) <- unlist(pheno[1,])
+    pheno <- pheno[-1]
+} else {
+    names(pheno) <- c("fid","iid",paste0("y",1:(ncol(pheno)-2)))
+}
+
+pheno$fatid <- 0
+pheno$matid <- 0
+n = ncol(pheno)
+pheno <- pheno[,c(1,2,n-1,n,3:(n-2)),with=FALSE]
+write.table(pheno, outname, sep = "\t", row.names = FALSE, quote = FALSE)
+
