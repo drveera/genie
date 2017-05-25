fe68fc4bdb10154aa8c5e7a7a38a658b5de36f70
drveera <drveerammc@gmail.com>
2017-05-25 23:07:57 +0200

    clump

---
+++ modules/sumstats/clump/clump.py
@@ -0,0 +1,30 @@
+#!/bin/python
+
+'''
+
+usage:
+ sumstats clump [options] --sumstats=FILE --ld=PLINKFILE
+
+options:
+ --sumstats=FILE     munged summary statistics file 
+ --out=PREFIX        output name prefix [default: clumped]
+ --ld=PLINKFILE      plink file without extension
+ --p1=NUMBER         pvalue threshold 1 [default: 1]
+ --p2=NUMBER         pvalue threshold 2 [default: 1]
+ --r2=NUMBER         rsquared value [default: 0.1]
+ --distance=NUMBER   window distance in kb [default: 250]
+ --nojob             if should run in front end
+ --njobs=NUMBER      Number of parallel jobs when running in front end
+ --dry-run           dry run snakemake
+
+'''
+
+from docopt import docopt
+import sys
+sys.path.insert(1, sys.path[0] + '/../../../library')
+import md
+from md import process_list
+
+arguments = docopt(__doc__)
+if __name__ == '__main__':
+    md.main(arguments,['clump'])
