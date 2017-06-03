3f54ba8130cd37d3a2050a630d852f338e3a2643
drveera <drveerammc@gmail.com>
2017-06-03 17:04:18 +0200

    update plink snake

--- modules/gwas/plink/famscript.R
+++ modules/gwas/plink/famscript.R
@@ -29,4 +29,4 @@ fampheno <- merge(fam,pheno,by=c("FID","IID"), sort = FALSE, all.x = TRUE)
 print(head(fampheno))
 fampheno <- fampheno[,c("FID","IID","V3","V4","V5","pheno")]
 fwrite(fampheno,outfile,sep="\t",na="NA")
-system(paste0("ln -s ", map.file, " ", dirname(outfile)))
+system(paste0("ln -s ", map.file, " ", dirname(outfile),"/."))
